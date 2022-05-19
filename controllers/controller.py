abbcac171a9be0a895043c19deead59a52de772b
Brian Kerr <briantkerr91@gmail.com>
2022-05-19 14:28:42 +0100

    started form

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,11 +1,15 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 from models.events_list import events
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title="Home")
 
 @app.route("/events")
 def show_events():
-    return render_template('index.html', title='Events List', events=events)
+    return render_template('events_list.html', title='Events List', events=events)
+
+@app.route("/add_event")
+def add_event():
+    return render_template("add_event.html", title="add event")
--- models/events_list.py
+++ models/events_list.py
@@ -1,6 +1,7 @@
 from models.events import Event
+from datetime import datetime
 
-event_1 = Event("19th May 2022", "Drinks after Lab", 20, "Pig and Butterfly", "Stress relief")
-event_2 = Event("19th August 2022", "Graduation", 30, "Pig and Butterfly", "Party time!")
+event_1 = Event(datetime(2022, 5, 19), "Drinks after Lab", 20, "Pig and Butterfly", "Stress relief")
+event_2 = Event(datetime(2022, 5, 16), "Graduation", 30, "Pig and Butterfly", "Party time!")
 
 events = [event_1, event_2]
