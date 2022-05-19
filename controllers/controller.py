feb5b1dae1778c1e46bf473c9ef0bb57612c3906
michaelmjh <74016689+michaelmjh@users.noreply.github.com>
2022-05-19 14:13:32 +0100

    Classes created

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,6 +1,11 @@
 from app import app
 from flask import render_template
+from models.events_list import events
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/events")
+def show_events():
+    return render_template('index.html', title='Events List', events=events)
---
+++ models/events_list.py
@@ -0,0 +1,6 @@
+from models.events import Event
+
+event_1 = Event("19th May 2022", "Drinks after Lab", 20, "Pig and Butterfly", "Stress relief")
+event_2 = Event("19th August 2022", "Graduation", 30, "Pig and Butterfly", "Party time!")
+
+events = [event_1, event_2]
